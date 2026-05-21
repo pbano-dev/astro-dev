350875860f035c46c7597979121b6b61837b01f0
Pablo Bano Benito <pablo.bano.benito@gmail.com>
2026-05-21 22:05:26 +0200

    Set canonical procution domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://pbano-dev-portfolio.netlify.app",
+  site: "https://pablobanobenito.com",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
